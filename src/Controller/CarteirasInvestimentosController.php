4dfc119e2a2169ece81de469eb0731b5f08dd519
leosk52 <leosk52@gmail.com>
2021-08-20 19:59:18 -0300

    graficoPat/fundo com rentab

--- src/Controller/CarteirasInvestimentosController.php
+++ src/Controller/CarteirasInvestimentosController.php
@@ -9,6 +9,7 @@ use DateTime;
 use DatePeriod;
 use DateInterval;
 use Cake\ORM\TableRegistry;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * CarteirasInvestimentos Controller
@@ -40,6 +41,13 @@ class CarteirasInvestimentosController extends AppController {
 		$data_list = array();
 		$datasDaCarteira = array();
 
+		
+		//$data1 = new DateTime('2011-09-11');
+		//$data2 = new DateTime('2011-10-13');
+		//$intervalo = $data1->diff($data2);
+		//var_dump($intervalo->days);
+		//echo $intervalo->format('%R%a dias');
+
 		foreach ($carteirasInvestimento->operacoes_financeiras as $operacoes) :
 			$datasDaCarteira[] = $operacoes->data;
 		endforeach;
@@ -48,14 +56,32 @@ class CarteirasInvestimentosController extends AppController {
 		$datasDaCarteira = array_unique($datasDaCarteira);
 		
 		$primeira_data = date('Y/m/d', $datasDaCarteira[0]->getTimestamp());
-		$segunda_data = date('Y/m/d');
+		//$dataaux = $datasDaCarteira[0];
+		//var_dump($dataaux);
+		$segunda_data = '2021/06/30';
+		//$segunda_dataa = new DateTime('2021/06/30'); //date('Y/m/d', '30/06/2021');
+		//$dif = $dataaux->diff($segunda_dataa);
+		//var_dump($dif->days);
+
 
 		while (strtotime($primeira_data) < strtotime($segunda_data)) :
+		//$i = 0;
+		//while ($i < $dif->days) :
 			$data_list[] = date('d/m/Y', strtotime($primeira_data));
-			$primeira_data = date('Y/m/d', strtotime("$primeira_data +1 day"));			
+			$primeira_data = date('Y/m/d', strtotime("$primeira_data +1 day"));		
+		//	$i++;	
 		endwhile;
 
 		return $data_list;
+	}
+
+	public function calcula_primeira_data($carteirasInvestimento) {
+		foreach ($carteirasInvestimento->operacoes_financeiras as $operacoes) :
+			$datasDaCarteira[] = $operacoes->data;
+		endforeach;
+		sort($datasDaCarteira);
+
+		return $datasDaCarteira[0];
 	}
 	
 	/**
@@ -79,6 +105,11 @@ class CarteirasInvestimentosController extends AppController {
 		if (sizeof($carteirasInvestimento->operacoes_financeiras) == 0) {
 			return;
 		}
+
+		// Preciso inicializar todas variaveis antes do for de cada op
+		$datas_totais = $this->calcula_datas_carteira($carteirasInvestimento);
+		//var_dump($datas_totais[0]);
+		$rentabilidade_fundo = [];
 		
 		$busca_tipo_operacao = $this->CarteirasInvestimentos->OperacoesFinanceiras->TipoOperacoesFinanceiras->find('all');
 		// saber qual tipo de operacao para somar/subtrair no patrimonio total // mudar o retorno, atual = bool
@@ -88,27 +119,25 @@ class CarteirasInvestimentosController extends AppController {
 			$tipo_de_operacao[$tipo_operacao['id']] = $tipo_operacao['is_aplicacao'];
 		}
 
-		//var_dump($tipo_de_operacao);
-
 		// Calcula patrimonio total de uma carteira e dos fundos individuais
 		$patrimonio_por_fundo = array(); //patrimonio[fundo] = valorTotalPorFundo
 		$id_fundo_unique = array();	
 		$patrimonio_total = [];
 		$data_anterior = 0;
-		
-		$dataQueVouUsar = $this->calcula_datas_carteira($carteirasInvestimento);
-		
+				
 		foreach ($carteirasInvestimento->operacoes_financeiras as $operacoes) :
 
 			$id_fundo_unique[] = $operacoes->cnpj_fundo_id;
 
 			$auxiliar_data = (string) $operacoes->data;
+			$auxiliar_fundo = $operacoes->cnpj_fundo_id;
+			$valor_total = $operacoes["valor_total"];
 
 			// se a operacao eh de deposito acrescenta, outros tipos diminui
 			if ($tipo_de_operacao[$operacoes['tipo_operacoes_financeira_id']] == 1) {
-				$patrimonio_total[$auxiliar_data][$operacoes->cnpj_fundo_id] += $operacoes["valor_total"];
+				$patrimonio_total[$auxiliar_data][$auxiliar_fundo] += $valor_total;
 			} else {
-				$patrimonio_total[$auxiliar_data][$operacoes->cnpj_fundo_id] -= $operacoes["valor_total"];
+				$patrimonio_total[$auxiliar_data][$auxiliar_fundo] -= $valor_total;
 			}
 			
 			//$patrimonio_por_fundo[$operacoes->cnpj_fundo_id] += $operacoes["valor_total"];
@@ -117,85 +146,81 @@ class CarteirasInvestimentosController extends AppController {
 
 		endforeach;
 
-		$id_fundo_unique = array_unique($id_fundo_unique);	
+		$id_fundo_unique = array_unique($id_fundo_unique);
+
+		$primeira_data = $this->calcula_primeira_data($carteirasInvestimento);
+
+
+		//var_dump($primeira_data);
+		$aux = date('Y/m/d', $primeira_data->getTimestamp());
+		//var_dump($aux);
+
+		// busca rentabilidade
+		foreach ($id_fundo_unique as $fundo_id) {
+
+			$busca_rentabilidade = $this->CarteirasInvestimentos->OperacoesFinanceiras->CnpjFundos->DocInfDiarioFundos->find('all',
+		 		['order' => ['DT_COMPTC' => 'ASC']])->where(['cnpj_fundo_id' => $fundo_id, 'DT_COMPTC >=' => $aux]);
+
+			foreach ($busca_rentabilidade as $busca) {
+				$data = date('d/m/Y', $busca['DT_COMPTC']->getTimestamp());
+				$rentabilidade_fundo[$data][$fundo_id] = $busca['rentab_diaria'];
+			}
+		}
+
+		//var_dump($rentabilidade_fundo);
+
+		//var_dump($patrimonio_total);
+		
 		
 		$patrimonio_view_fundo = [];		
 		
+		// pra cada fundo sem repetidos, preenche o array para exibir no grafico
+		//
 		$exibe = array("['Data', 'Patrimônio Líquido Total', ");
 		foreach ($id_fundo_unique as $fundoId) {
 			$patrimonio_view_fundo[$fundoId] = 0;
 			$exibe[] = $exibe[count($exibe)-1] . "'Fundo " . (string) $fundoId . "', ";
 		}
-
 		$exibeTudo[] = $exibe[count($exibe)-1] . "],";
-		//var_dump($exibeTudo);
-		//$exibe = array_unique($exibe);
-		//$exibe[] = "['Data', 'Patrimônio Líquido Total', $fundoAAA],";
-		//$exibe = array("['Data', 'Patrimônio Líquido Total', 'fundo1', 'fundo2', 'fundo3', 'fundo4', 'fundo5', 'fundo6', 'fundo7', 'fundo8', ],");
-		//var_dump($exibe);
-		//$tam = count($patrimonio_view_fundo);
-		//$exibe = array("['Data', 'Patrimônio Líquido Total', $tam, ],");
-		//$exibe2 = array();
+		//
 
 		$patrimonio_view = [];
 		$aux = 0;
-		foreach ($dataQueVouUsar as $data) {
-			foreach ($patrimonio_total[(string)$data] as $fundoId => $patrimonio) {
-				//$aux = $fundoId;
-				$patrimonio_view_fundo[$fundoId] += $patrimonio; //patrimonio[38733] = 
-				$patrimonio_view["total"] += $patrimonio;
+		// fors pra exibir os valores no grafico patrimonio
+		$data_anterior = '';
+		foreach ($datas_totais as $data) {
+			//$soma = 0;
+			foreach ($id_fundo_unique as $fundo_id) {
+				$rendimento_dia = $patrimonio_total[$data_anterior][$fundo_id] * $rentabilidade_fundo[$data][$fundoId]; // 5000 20/01/2020 * 0.0013
+				$patrimonio_dia_anterior = $patrimonio_total[$data_anterior][$fundo_id] + $rendimento_dia; // 5000 19/01/2020 * 0.00005
+				$patrimonio_total[$data][$fundo_id] += $patrimonio_dia_anterior; // 5000, 5000 + (5000*0.00005)
+				//var_dump($rendimento_dia);
+				//var_dump($patrimonio_dia_anterior);
+				//var_dump($patrimonio_total[$data][$fundoId]);
+
+				$patrimonio_view_fundo[$fundo_id] += $rendimento_dia;
+				$patrimonio_view["total"] += $rendimento_dia;
+				//$soma += $patrimonio_total[$data][$fundo_id];
+				//$patrimonio_view_fundo[$fundoId] += $patrimonio * $rentabilidade_fundo[$data][$fundoId]; // patrimonio POR FUNDO
+				//$patrimonio_view["total"] += $patrimonio_total[$data][$fundoId]; // TOTAL
+
 			}
+			//$patrimonio_view_fundo[$fundoId] += $patrimonio_view["total"];
+				
 			$exibeTudo[] = "['" . (string)$data . "', " . $patrimonio_view["total"];
-
-			foreach ($id_fundo_unique as $fundo) {
+			$tamanho = count($exibeTudo) - 1;
+			foreach ($id_fundo_unique as $op) {
 				// concatenar virgula e o valor do fundo na ultima linha do exibe
-				$exibeTudo[count($exibeTudo) - 1] = $exibeTudo[count($exibeTudo) - 1] . ", " . $patrimonio_view_fundo[$fundo];
+				$exibeTudo[$tamanho] = $exibeTudo[$tamanho] . ", " . $patrimonio_view_fundo[$op];
 			}
-			$exibeTudo[count($exibeTudo) - 1] = $exibeTudo[count($exibeTudo) - 1] . "],";
+			$exibeTudo[$tamanho] = $exibeTudo[$tamanho] . "],";
+
+			//$patrimonio_total[$data]['Total'] += $soma;
+			$data_anterior = $data;
+			//var_dump($data_anterior);
 		}
 
-		//var_dump($exibe);
-
-
-
-		/*
-		$patrimonio_view = [];
-		$exibe_fundo = [];
-		$aux = 0;
-		foreach ($dataQueVouUsar as $data) {
-			foreach ($patrimonio_total[(string)$data] as $fundoId => $patrimonio) {
-				$aux = $fundoId;
-				$patrimonio_view_fundo[$fundoId] += $patrimonio; //patrimonio[38733] = 
-				$patrimonio_view["total"] += $patrimonio;
-			}
-			$exibe[] = "['" . (string)$data . "', " . $patrimonio_view["total"] . ",";
-
-			foreach ($id_fundo_unique as $fundo) {
-				// concatenar virgula e o valor do fundo na ultima linha do exibe
-				$exibe[count($exibe) - 1] = $exibe[count($exibe) - 1] . " [" . $patrimonio_view_fundo[$fundo] . "],";
-			}
-		}
-		*/
 		
-		//var_dump($array_de_fundos);
-
-		/*
-		$valorDif = [];
-		foreach ($dataQueVouUsar as $data) {
-			foreach ($rentab_data_fundo[$data] as $fundoId => $rentab) {
-				var_dump($rentab);
-				//$patrimonio_view[$fundoId] += $patrimonio;
-				$valorDif["total"] += $rentab;
-				//$aux = $fundoId;
-			}
-			//$exibe[] = "['" . (string)$data . "'," . $patrimonio_view["total"] . $patrimonio_view[$aux] . ",";
-			$exibe2[] = "['" . (string)$data . "'," . $valorDif["total"] . "],";
-			//var_dump($exibe2);
-			//foreach ($id_fundo_unique as $fundoId) {
-				// $exibe[count($exibe)-1] += $patrimonio_view[$fundoId] . ",";
-			//}
-		}
-		*/
 		
 		$this->set(compact('exibe', 'exibe2', 'id_fundo_unique', 'exibeTudo'));
 		
